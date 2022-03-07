20608d35b1c8cee2f798141dc0d842a6dfa8f80d
Kepler Sticka-Jones <kepler@stickajones.org>
2022-03-07 13:13:07 -0700

    fix: convert to esm
    BREAKING CHANGE: at least node 12 is needed

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-module.exports = {
+import remarkRetext from "remark-retext";
+import { unified } from "unified";
+import retextEnglish from "retext-english";
+import retextSyntaxMentions from "retext-syntax-mentions";
+import retextSyntaxUrls from "retext-syntax-urls";
+import retextSpell from "retext-spell";
+import dictionary from "dictionary-en";
+import retextContractions from "retext-contractions";
+import retextDiacritics from "retext-diacritics";
+import retextEquality from "retext-equality";
+import retextIndefiniteArticle from "retext-indefinite-article";
+import retextIntensify from "retext-intensify";
+import retextPassive from "retext-passive";
+import retextProfanities from "retext-profanities";
+import retextReadability from "retext-readability";
+import retextRedundantAcronyms from "retext-redundant-acronyms";
+import retextRepeatedWords from "retext-repeated-words";
+import retextSentenceSpacing from "retext-sentence-spacing";
+import retextSimplify from "retext-simplify";
+
+export default {
   plugins: [
     [
-      require("remark-retext"),
-      require("unified")()
-        .use(require("retext-english"))
-        .use(require("retext-syntax-mentions"))
-        .use(require("retext-syntax-urls"))
-        .use(require("retext-spell"), require("dictionary-en"))
-        .use(require("retext-contractions"))
-        .use(require("retext-diacritics"))
-        .use(require("retext-equality"))
-        .use(require("retext-indefinite-article"))
-        .use(require("retext-intensify"))
-        .use(require("retext-passive"))
-        .use(require("retext-profanities"))
-        .use(require("retext-readability"))
-        .use(require("retext-redundant-acronyms"))
-        .use(require("retext-repeated-words"))
-        .use(require("retext-sentence-spacing"))
-        .use(require("retext-simplify")),
+      remarkRetext,
+      unified()
+        .use(retextEnglish)
+        .use(retextSyntaxMentions)
+        .use(retextSyntaxUrls)
+        .use(retextSpell, dictionary)
+        .use(retextContractions)
+        .use(retextDiacritics)
+        .use(retextEquality)
+        .use(retextIndefiniteArticle)
+        .use(retextIntensify)
+        .use(retextPassive)
+        .use(retextProfanities)
+        .use(retextReadability)
+        .use(retextRedundantAcronyms)
+        .use(retextRepeatedWords)
+        .use(retextSentenceSpacing)
+        .use(retextSimplify),
     ],
   ],
 };
